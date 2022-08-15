190c500ea9465bcc70985fa032103f9a6244c55e
ma2-b <mohammadabdelqaderbusiness298@gmail.com>
2022-08-15 19:44:08 +0300

    add custom login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from . import views
 
+from django.contrib.auth.views import LogoutView
+
+
 app_name = 'accounts'
 
 urlpatterns = [
     
-    path('register/',views.UserRegisterView.as_view(),name='register')
-    
+    path('register/',views.UserRegisterView.as_view(), name='register'),
+    path('login/',views.CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
+
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
-from django.urls import reverse, reverse_lazy
 from django.contrib.auth.views import PasswordChangeView
 
+from django.contrib.auth.views import LoginView
 from .forms import SingUpForm
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('market:home')
+
 
 class UserRegisterView(generic.CreateView):
     form_class = SingUpForm 
